7a8807d95d771a863551274519ccf9716c4ec881
BrenoLUCO <cordeirobreno4@yahoo.com.br>
2025-06-18 02:16:42 -0300

    Atualizando schema_metricas

--- backend/src/schemas/metricas.schema.ts
+++ backend/src/schemas/metricas.schema.ts
@@ -2,7 +2,8 @@ import { number, z } from "zod";
 
 // Schemas
 export const MetricasSchema = z.object({
-    id: z.string().max(50),                      // ID da reserva                     
+    id: z.string().max(50),                      // ID da reserva
+    id_stays: z.string().max(50),                // ID do stay                     
     ticket_diaria: z.number(),                   // Valor da diária por imóvel
     receita_com_taxas: z.number(),               // Receita total incluindo taxas
     taxas: z.number(),
